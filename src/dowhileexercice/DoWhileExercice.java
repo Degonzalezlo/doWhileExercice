9930afdeb2b650802675c45946baa3b3473c7007
Daniel <danielgonzalez.l@hotmail.com>
2025-03-07 10:12:03 -0500

    add the project

---
+++ src/dowhileexercice/DoWhileExercice.java
@@ -0,0 +1,16 @@
+
+package dowhileexercice;
+
+
+public class DoWhileExercice {
+
+   
+    public static void main(String[] args) {
+        int contador =0;
+        do{
+            System.out.println("estoy en vuelta # "+ (contador + 1));
+            contador ++;
+        }while(contador <10);
+    }
+    
+}
